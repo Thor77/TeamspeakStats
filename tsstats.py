029a2476f9173f0a99114d438729b8bd8ec2cd9c
Thor77 <xXThor77Xx@gmail.com>
2015-03-12 21:00:53 +0100

    Remove MY piwik-tracking code :scream:

--- tsstats.py
+++ tsstats.py
@@ -132,21 +132,6 @@ output.append('''
         <style>
         h1 {text-align: center;}
         </style>
-<!-- Piwik -->
-<script type="text/javascript">
-  var _paq = _paq || [];
-  _paq.push(['trackPageView']);
-  _paq.push(['enableLinkTracking']);
-  (function() {
-    var u="//analytics.crapwa.re/";
-    _paq.push(['setTrackerUrl', u+'piwik.php']);
-    _paq.push(['setSiteId', 1]);
-    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
-  })();
-</script>
-<noscript><p><img src="//analytics.crapwa.re/piwik.php?idsite=1" style="border:0;" alt="" /></p></noscript>
-<!-- End Piwik Code -->
     </head>
     <body>
 ''')
