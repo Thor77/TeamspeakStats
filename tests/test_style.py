266ff582db80b98dfc323ab1e2522d4f4f250658
Thor77 <xXThor77Xx@gmail.com>
2015-07-31 18:59:07 +0200

    use glob to select files for stylecheck

--- tests/test_style.py
+++ tests/test_style.py
@@ -1,8 +1,11 @@
 import pep8
+from glob import glob
 
 
 def test_pep8_comformance():
     ''' Test that the code conforms to PEP8 '''
     pep8style = pep8.StyleGuide(config_file='.pep8')
-    result = pep8style.check_files(['tsstats.py', 'tests/test_config.py', 'tests/test_general.py', 'tests/test_style.py'])
+    files = glob('tests/*.py')
+    files.append('tsstats.py')
+    result = pep8style.check_files(files)
     assert result.total_errors == 0
