b99b86f4fd602a88b8f59688c4dcc37fcc3cf5ab
Thor77 <xXThor77Xx@gmail.com>
2015-07-31 22:53:09 +0200

    modify ident_map_tests to match real json

--- tests/test_ident_map.py
+++ tests/test_ident_map.py
@@ -1,8 +1,8 @@
 from tsstats import Clients, Client
 
 ident_map = {
-    1: 2,
-    5: 2,
+    '1': '2',
+    '5': '2',
     'UID1': 'UID2',
     'UID5': 'UID2'
 }
@@ -10,8 +10,8 @@ clients = Clients(ident_map)
 
 
 def test_get_id():
-    assert clients[1].identifier == 2
-    assert clients[5].identifier == 2
+    assert clients['1'].identifier == '2'
+    assert clients['5'].identifier == '2'
 
 
 def test_get_uid():
