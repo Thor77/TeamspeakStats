9acf33dea89d9d81873398d0ca5955ebf5e16b58
Thor77 <xXThor77Xx@gmail.com>
2016-05-08 18:04:56 +0200

    remove redundant pep8-style-test

--- tests/test_style.py
+++ tests/test_style.py
@@ -1,19 +1,7 @@
-from glob import glob
-
-import pep8
-from pyflakes.api import checkPath
-
-
-def test_pep8_comformance():
-    ''' Test that the code conforms to PEP8 '''
-    pep8style = pep8.StyleGuide(config_file='.pep8')
-    files = glob('tests/*.py')
-    files.append('tsstats.py')
-    result = pep8style.check_files(files)
-    assert result.total_errors == 0
+import pyflakes.api
 
 
 def test_pyflakes():
     ''' Test that at least tsstats.py conforms to PyFlakes '''
-    result = checkPath('tsstats.py')
+    result = pyflakes.api.checkPath('**/*.py')
     assert result == 0
