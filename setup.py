432e89bfbe50e0e7559f529abe01b7ecea9e3114
Thor77 <xXThor77Xx@gmail.com>
2016-05-11 21:09:45 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='tsstats',
+    version='0.1.0',
+    author='Thor77',
+    author_email='thor77@thor77.org',
+    description='A simple Teamspeak stats-generator',
+    keywords='ts3 teamspeak teamspeak3 tsstats teamspeakstats',
+    url='https://github.com/Thor77/TeamspeakStats',
+    packages=['tsstats'],
+    entry_points={
+        'console_scripts': [
+            'tsstats = tsstats.__main__:cli'
+        ]
+    },
+    install_requires=open('requirements.txt').read()
+)
