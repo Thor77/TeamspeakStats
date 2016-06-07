41fee91e631ec0819591e788ef126fcd14a17129
Thor77 <xXThor77Xx@gmail.com>
2016-06-07 17:59:02 +0200

    add tsstats/template.html to setup.py:package_data
    * fix TemplateNotFound on pip-installations
    * bump version to 0.6.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='tsstats',
-    version='0.6.0',
+    version='0.6.1',
     author='Thor77',
     author_email='thor77@thor77.org',
     description='A simple Teamspeak stats-generator',
@@ -13,6 +13,9 @@ setup(
         'console_scripts': [
             'tsstats = tsstats.__main__:cli'
         ]
+    },
+    package_data={
+        'tsstats': ['template.html']
     },
     install_requires=[
         'Jinja2>=2.8'
