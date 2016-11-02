edc914b451d6f2015b9fdce9dd68d2223f031d0d
Thor77 <thor77@thor77.org>
2016-11-02 21:24:36 +0100

    Bump version to 0.12.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='tsstats',
-    version='0.12.4',
+    version='0.12.5',
     author='Thor77',
     author_email='thor77@thor77.org',
     description='A simple Teamspeak stats-generator',
