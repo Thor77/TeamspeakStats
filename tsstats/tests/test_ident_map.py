5968dc31ddb55ce2ba47fb2fee50641f95fee498
Matthew <git@matthewgamble.net>
2017-02-19 00:58:23 +1100

    Support for a nicer structure for ID maps (#12)
    This adds support for a more expressive (albeit more verbose) IdentMap
    structure. It makes it easier to annotate the structure with additional
    data (such as names to associate with the IDs), to assist with
    maintaining the IdentMap.

--- tsstats/tests/test_ident_map.py
+++ tsstats/tests/test_ident_map.py
@@ -1,6 +1,7 @@
 import pytest
 
 from tsstats.client import Client, Clients
+from tsstats.utils import transform_pretty_identmap
 
 
 @pytest.fixture(scope='module')
@@ -24,3 +25,25 @@ def test_ident_map(identmap_clients):
     assert clients['5'] == cl
     assert clients['UID1'] == uidcl
     assert clients['UID5'] == uidcl
+
+
+@pytest.mark.parametrize('test_input,expected', [
+    (
+        [
+            {'primary_id': '1', 'alternate_ids': ['3', '6']},
+            {'primary_id': '4', 'alternate_ids': ['9', '42', '23']}
+        ],
+        (('3', '1'), ('6', '1'), ('9', '4'), ('42', '4'), ('23', '4'))
+    ),
+    (
+        [
+            {'name': 'Friend 1', 'primary_id': '2', 'alternate_ids': ['4']},
+            {'name': 'Friend 3', 'primary_id': '8', 'alternate_ids': ['9', '14']}
+        ],
+        (('4', '2'), ('9', '8'), ('14', '8'))
+    )
+])
+def test_transform_pretty_identmap(test_input, expected):
+    transformed_identmap = transform_pretty_identmap(test_input)
+    for alternate, primary in expected:
+        assert transformed_identmap[alternate] == primary
