962fd486afe25031d5fb6332f623e970b694b321
Thor77 <thor77@thor77.org>
2017-02-23 21:38:59 +0100

    Test reading config from disk again

--- tsstats/tests/test_config.py
+++ tsstats/tests/test_config.py
@@ -18,3 +18,12 @@ def test_config(config):
     assert config.get('General', 'log') == 'tsstats/tests/res/test.log'
     config.set('General', 'output', 'output.html')
     assert config.get('General', 'output') == 'output.html'
+
+
+def test_read():
+    config = load(path='tsstats/tests/res/config.ini')
+    # test defaults
+    assert not config.getboolean('General', 'debug')
+    # test written values
+    assert config.get('General', 'log') == 'tsstats/tests/res/test.log'
+    assert config.get('General', 'output') == 'tsstats/tests/res/output.html'
